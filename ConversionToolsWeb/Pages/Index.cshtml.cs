dc8cfd49d7e403381af25009b37617ed952dd07b
Eric Spencer <eric@spenc.org>
2025-11-23 20:49:09 -0500

    WIP binding GUI

--- ConversionToolsWeb/Pages/Index.cshtml.cs
+++ ConversionToolsWeb/Pages/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using ConversionToolsWeb.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,14 +6,29 @@ namespace ConversionToolsWeb.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ITimeZoneInfoResolver _timeZoneInfoResolver;
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+
+        public IndexModel(ILogger<IndexModel> logger, ITimeZoneInfoResolver timeZoneInfoResolver)
         {
             _logger = logger;
+            _timeZoneInfoResolver = timeZoneInfoResolver;
         }
 
-        public void OnGet()
+
+        public IEnumerable<string> SupportedTimeZones => _timeZoneInfoResolver.SupportedTimeZones;
+        
+        [BindProperty]
+        public string? DateEntry { get; set; }
+
+        [BindProperty]
+        public long? TicksEntry { get; set; }
+
+        [BindProperty]
+        public string? SelectedTimeZone { get; set; }
+
+        public void OnPostConvert()
         {
 
         }
