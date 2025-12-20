dd86e54a6296f0f44939099fec8f5075a6a36f20
Eric Spencer <eric@spenc.org>
2025-12-20 13:24:52 -0500

    wip swagger

--- ConversionToolsWeb/Program.cs
+++ ConversionToolsWeb/Program.cs
@@ -7,12 +7,30 @@ namespace ConversionToolsWeb
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddControllers();
+
+            // Add services to the container.
             builder.Services.AddRazorPages();
+
+            // Register Swagger services
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             DISetup.ConfigureServices(builder.Services);
 
 
             var app = builder.Build();
+
+            app.UseSwagger(); // Enable middleware to serve generated Swagger as a JSON endpoint
+            app.UseSwaggerUI(); // Enable middleware to serve Swagger UI
+
+            //app.UseSwaggerUI(options =>
+            //{
+            //    options.SwaggerEndpoint("/api/swagger/v1/swagger.json", "API V1");
+            //    options.RoutePrefix = "api/swagger";
+            //}
+            //); // Enable middleware to serve Swagger UI
+
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
