ec941e1594c65bb24ee4ac3bd03d39009078942f
Eric Spencer <eric@spenc.org>
2025-12-20 14:13:59 -0500

    Working swagger docs in relative URL

--- ConversionToolsWeb/Program.cs
+++ ConversionToolsWeb/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi;
+
 namespace ConversionToolsWeb
 {
     public class Program
@@ -12,26 +14,12 @@ namespace ConversionToolsWeb
             // Add services to the container.
             builder.Services.AddRazorPages();
 
-            // Register Swagger services
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
+            SwaggerSetup.InitializeSwaggerService(builder.Services);
             DISetup.ConfigureServices(builder.Services);
-
 
             var app = builder.Build();
 
-            app.UseSwagger(); // Enable middleware to serve generated Swagger as a JSON endpoint
-            app.UseSwaggerUI(); // Enable middleware to serve Swagger UI
-
-
-            //app.UseSwaggerUI(options =>
-            //{
-            //    options.SwaggerEndpoint("/api/swagger/v1/swagger.json", "API V1");
-            //    options.RoutePrefix = "api/swagger";
-            //}
-            //); // Enable middleware to serve Swagger UI
-
+            SwaggerSetup.InitializeSwaggerMiddleware(app);
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
---
+++ ConversionToolsWeb/SwaggerSetup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.OpenApi;
+using System.Runtime.CompilerServices;
+
+namespace ConversionToolsWeb
+{
+    public static class SwaggerSetup
+    {
+        private const string Version = "v1";
+        private const string Title = "Dev Tools API";
+        private const string RelativeEndpoint = "api-docs";
+        private const string RelativeEndpointRooted = "/" + RelativeEndpoint;
+        private const string SwaggerJsonEndpointName = "swagger.json";
+
+        public static void InitializeSwaggerService(IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(
+                o => o.SwaggerDoc(Version, new OpenApiInfo
+                {
+                    Title = Title,
+                    Version = Version
+                }
+                ));
+        }
+
+        public static void InitializeSwaggerMiddleware(WebApplication app)
+        {
+            app.UseSwagger(o => o.RouteTemplate = RelativeEndpoint + "/{documentName}/" + SwaggerJsonEndpointName); // Enable middleware to serve generated Swagger as a JSON endpoint
+            app.UseSwaggerUI(o =>
+            {
+                o.SwaggerEndpoint($"{RelativeEndpointRooted}/{Version}/{SwaggerJsonEndpointName}", Title);
+                o.RoutePrefix = RelativeEndpoint;
+                o.DocumentTitle = Title;
+            }
+            ); // Enable middleware to serve Swagger UI
+        }
+    }
+}
