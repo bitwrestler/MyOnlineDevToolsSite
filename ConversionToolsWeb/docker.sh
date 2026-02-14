08b85eaa9c3dc77045cd0104f4f39f752bef9a4c
eric <eric@spenc.org>
2026-02-14 09:31:39 -0500

    fix deploy docker script to replace the docker container correctly

--- ConversionToolsWeb/docker.sh
+++ ConversionToolsWeb/docker.sh
@@ -1,4 +1,5 @@
-!#/bin/sh
+!#/usr/bin/bash
 LABEL=conversion_tools_web
-docker build -t $LABEL -f Dockerfile . &&  docker run -d -p 5001:8080 $LABEL
+docker rm -f $LABEL
+docker build -t $LABEL -f Dockerfile . &&  docker run -d -p 5001:8080 --name $LABEL $LABEL
 
