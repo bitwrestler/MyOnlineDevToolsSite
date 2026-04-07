5016daf52cfde75ed295542d745fb71a2833bf74
eric <eric@spenc.org>
2026-04-06 20:21:27 -0400

    fix deployment

--- ConversionToolsWeb/azure_deploy.sh
+++ ConversionToolsWeb/azure_deploy.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-dotnet publish -c Release -o ./publish && zip publish.zip ./publish/* && az webapp deploy --resource-group WebAppResourceGroup --name DateTimeConversionTools --type zip --src-path ./publish.zip && rm publish.zip
+
+dotnet publish -c Release -o ./publish && \
+ cd ./publish && \
+ zip ../publish.zip -r ./* && \
+  cd ..
+  az webapp deploy --resource-group WebAppResourceGroup --name DateTimeConversionTools --type zip --src-path ./publish.zip && \
+   rm ./publish.zip
