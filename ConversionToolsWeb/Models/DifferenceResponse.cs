7e623a4cec14d1706cb00d37b1805f599704636c
eric <eric@spenc.org>
2026-05-23 07:57:45 -0400

    diff response

---
+++ ConversionToolsWeb/Models/DifferenceResponse.cs
@@ -0,0 +1,11 @@
+namespace ConversionToolsWeb.Models;
+
+public class DifferenceResponse
+{
+    public double Months { get; set; }
+    public double Days { get; set; }
+    public double Hours { get; set; }
+    public double Minutes { get; set; }
+    public double Seconds { get; set; }
+    public long Ticks { get; set; }
+}
