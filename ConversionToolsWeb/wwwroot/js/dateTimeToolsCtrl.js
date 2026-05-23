3c3c484ea5632dc9a9e98feee5697e593dd29c8c
eric <eric@spenc.org>
2026-05-23 19:24:08 -0400

    todo

--- ConversionToolsWeb/wwwroot/js/dateTimeToolsCtrl.js
+++ ConversionToolsWeb/wwwroot/js/dateTimeToolsCtrl.js
@@ -217,8 +217,6 @@ function convertTicksGreater(ticks1, ticks2, callback) {
     makePostRequest(url, model, callback);
 }
 
-//TODO get enabling/diabling and clearing to work for date/time diff
-
 function convertDateTimeDiff(dt1,dt2,timeZone)
 {
     let ctrlData = _getControlIds(convertTypes.DateTimeDifference);
